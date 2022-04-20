72eae78c6deb51d862caf7f8c948c66ad7b0a312
Nicholas Wehr <github@biochx.com>
2022-04-20 15:39:52 -0700

    ported typescript to javascript

--- packages/gatsby-plugin-support-chat/src/utils/cache.js
+++ packages/gatsby-plugin-support-chat/src/utils/cache.js
@@ -3,6 +3,6 @@ import * as path from "path"
 import os from "os"
 
 export function cacheLoad(cacheName) {
-  const cachePath = path.join(os.tmpdir(), "./gatsby-plugin-support-chat/")
-  return flatCache.load(cacheName, cachePath)
+    const cachePath = path.join(os.tmpdir(), "./gatsby-plugin-support-chat/")
+    return flatCache.load(cacheName, cachePath)
 }
